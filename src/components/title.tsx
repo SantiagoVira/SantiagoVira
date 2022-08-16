139df34c8ca24e52ae9dcdc6d511e61a88b44b31
SantiagoVira <santiagovira06@gmail.com>
2022-08-16 15:55:59 -0400

    Add meta tags

---
+++ src/components/title.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const Title: React.FC<{ page?: string }> = ({ page = "" }) => {
+  return (
+    <Head>
+      <title>{page && `${page} | `}Santiago Vira</title>
+      <meta property="og:title" content="Santiago Vira" />
+      <meta property="og:site_name" content="Santiago" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://santiagovira.netlify.app" />
+      <meta content="summary_large_image" property="twitter:card" />
+      <meta content="/astrius_cover.png" property="og:image" />
+      <meta
+        property="og:description"
+        content="Rising highschool junior and frontend designer with a broken sleep schedule and a computer."
+      />
+      <meta name="theme-color" content="#D53F8C" />
+    </Head>
+  );
+};
+
+export default Title;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Heading } from "@chakra-ui/react";
 import Navbar from "@components/navigation/navbar";
+import Title from "@components/title";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Box>
+      <Title />
       <Navbar />
     </Box>
   );
