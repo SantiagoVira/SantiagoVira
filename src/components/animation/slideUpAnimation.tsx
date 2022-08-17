421ed017c67ff5066536dfc36637fb9a85f64e04
SantiagoVira <santiagovira06@gmail.com>
2022-08-16 23:55:36 -0400

    fix colors for transition

--- src/components/animation/slideUpAnimation.tsx
+++ src/components/animation/slideUpAnimation.tsx
@@ -21,12 +21,10 @@ export default function SlideUpAnimation() {
   const lineVariants: Variants = {
     hidden: {
       y: "200%",
-      color: "#0055FF",
       transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 },
     },
     visible: {
       y: 0,
-      color: "#FF0088",
       transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 },
     },
   };
