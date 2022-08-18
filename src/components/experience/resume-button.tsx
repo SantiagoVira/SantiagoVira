6e246edd1ef48419077b44a784b749b8bc4fabdc
SantiagoVira <santiagovira06@gmail.com>
2022-08-18 01:42:18 -0400

    please fix colors

--- src/components/experience/resume-button.tsx
+++ src/components/experience/resume-button.tsx
@@ -4,7 +4,10 @@ const ResumeButton: React.FC = () => {
   return (
     <Button
       as={Link}
-      background="whiteAlpha.200"
+      background="whiteAlpha.200 !important"
+      backgroundColor="whiteAlpha.200 !important"
+      bg="whiteAlpha.200 !important"
+      bgColor="whiteAlpha.200 !important"
       href="/Santiago_Vira_Resume.pdf"
       download>
       Download my resume
