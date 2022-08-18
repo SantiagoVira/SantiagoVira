d6cc6c203a71f8a67b30dd430eeed0aacbaa64e6
SantiagoVira <santiagovira06@gmail.com>
2022-08-18 01:35:40 -0400

    fix deploy colors

--- src/components/experience/resume-button.tsx
+++ src/components/experience/resume-button.tsx
@@ -2,7 +2,11 @@ import { Button, Link } from "@chakra-ui/react";
 
 const ResumeButton: React.FC = () => {
   return (
-    <Button as={Link} href="/Santiago_Vira_Resume.pdf" download>
+    <Button
+      as={Link}
+      background="whiteAlpha.200"
+      href="/Santiago_Vira_Resume.pdf"
+      download>
       Download my resume
     </Button>
   );
