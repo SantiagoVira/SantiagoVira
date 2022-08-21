86936c71655e969acde0abdc7c7bfbbb5ca7e92d
SantiagoVira <santiagovira06@gmail.com>
2022-08-21 00:31:08 -0400

    Finish project card

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -1,4 +1,13 @@
-import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { projectDataType } from "@components/cms/cms-data";
 import PortableTextLayout from "@components/cms/portable-text";
 import NextLink from "next/link";
@@ -21,42 +30,68 @@ const Project: React.FC<projectDataType> = ({
       p="1.5rem"
       borderRadius="9px"
       flexDirection="column">
-      <Flex
-        alignItems="center"
-        justifyContent="center"
-        w="100%"
-        style={{ aspectRatio: "1/1" }}>
-        <Image
-          src={image}
-          alt={`${slug} logo`}
-          h="100%"
-          maxW="100%"
-          maxH="100%"
-        />
-      </Flex>
+      <Flex w="100%" h="100%" flexDirection="column">
+        <Flex alignItems="center" justifyContent="center" w="100%" mb="2rem">
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            w="80%"
+            style={{ aspectRatio: "1/1" }}>
+            {link ? (
+              <NextLink href={link} target="_blank" passHref>
+                <Link
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  gap="0.5rem"
+                  color="white"
+                  w="100%"
+                  h="100%">
+                  <Image
+                    src={image}
+                    alt={`${slug} logo`}
+                    h="100%"
+                    maxW="100%"
+                    maxH="100%"
+                  />
+                </Link>
+              </NextLink>
+            ) : (
+              <Image
+                src={image}
+                alt={`${slug} logo`}
+                h="100%"
+                maxW="100%"
+                maxH="100%"
+              />
+            )}
+          </Flex>
+        </Flex>
 
-      {link ? (
-        <NextLink href={link} target="_blank" passHref>
-          <Link display="flex" alignItems="center" gap="0.5rem" color="white">
-            <Text fontWeight="bold" fontSize="1.15rem">
-              {title}
-            </Text>
-          </Link>
-        </NextLink>
-      ) : (
-        <Text fontWeight="bold" fontSize="1.15rem">
-          {title}
+        {link ? (
+          <NextLink href={link} target="_blank" passHref>
+            <Link display="flex" alignItems="center" gap="0.5rem" color="white">
+              <Text fontWeight="bold" fontSize="1.7rem">
+                {title}
+              </Text>
+            </Link>
+          </NextLink>
+        ) : (
+          <Text fontWeight="bold" fontSize="1.7rem">
+            {title}
+          </Text>
+        )}
+        <Text fontWeight={600} color="extras.smallText">
+          {langString}
         </Text>
-      )}
-      <Text fontWeight={600} color="extras.smallText">
-        {langString}
-      </Text>
-      <PortableTextLayout text={body} />
+        <Divider mt="0.5rem" mb="0.75rem" />
+        <PortableTextLayout text={body} />
+      </Flex>
       {codeLink ? (
-        <NextLink href={codeLink} target="_blank" passHref>
-          <Link display="flex" alignItems="center" gap="0.5rem" color="white">
+        <NextLink href={codeLink} target="_blank" passHref style={{ flex: 1 }}>
+          <Button mt="1.5rem" p="1rem" as={Link}>
             View Source Code
-          </Link>
+          </Button>
         </NextLink>
       ) : (
         <></>
