b9bab1e1f4a1ada2b4b380870783b4e19357163d
SantiagoVira <santiagovira06@gmail.com>
2023-03-15 08:55:03 -0400

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "@theme/index";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme} cssVarsRoot="body">
         <Component {...pageProps} />
+        <Analytics />
       </ChakraProvider>
     </QueryClientProvider>
   );
