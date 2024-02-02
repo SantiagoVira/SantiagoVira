cdc47ff1aa993d94120cd8fd8f7f2d7622d85bdf
SantiagoVira <santiagovira06@gmail.com>
2024-02-02 00:44:54 -0500

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background`}>
-        <SmoothScroll>{children}</SmoothScroll>
+        <SmoothScroll>
+          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-start justify-between text-sm lg:flex">
+              {children}
+            </div>
+          </main>
+        </SmoothScroll>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="font-dunk text-8xl text-accent">SAN</h1>
-        <h1 className="font-dunk text-8xl text-accent ">TIAGO</h1>
-        <div className="h-[300vh]"></div>
-      </div>
-    </main>
+    <>
+      <h1 className="font-dunk text-8xl text-accent">SAN</h1>
+      <h1 className="font-dunk text-8xl text-accent ">TIAGO</h1>
+      <div className="h-[300vh]"></div>
+    </>
   );
 }
