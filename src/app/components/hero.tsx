b9862d9c004630c7e12fd81f048197807529c83f
SantiagoVira <santiagovira06@gmail.com>
2024-02-03 19:12:30 -0500

    tidbits

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const tidbits = ["✧", "⏺", "☺︎", "✸"];
+
+const Hero: React.FC = () => {
+  return (
+    <div className="flex flex-col w-full max-w-[58rem] origin-top justify-center h-full scale-y-150">
+      <h1 className="font-dunk text-9xl mr-auto text-accent">
+        SAN <Tidbit />
+      </h1>
+      <h1 className="font-dunk text-9xl ml-auto text-accent ">
+        <Tidbit /> TIAGO
+        <br />
+        <span className="opacity-0">
+          _<br />_
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+const Tidbit: React.FC = () => {
+  const chooseRandom = () =>
+    tidbits[Math.floor(Math.random() * tidbits.length)];
+
+  const [symbol, setSymbol] = useState(chooseRandom());
+
+  useEffect(() => {
+    //Implementing the setInterval method
+    const interval = setInterval(() => {
+      setSymbol(chooseRandom());
+    }, 2500);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span className="scale-y-[0.66] inline-block">{symbol}</span>;
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
+import dynamic from "next/dynamic";
+const Hero = dynamic(() => import("@/components/hero"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col w-full max-w-[60rem] origin-top justify-center h-full scale-y-[2]">
-        <h1 className="font-dunk text-9xl mr-auto text-accent">SAN</h1>
-        <h1 className="font-dunk text-9xl ml-auto text-accent ">
-          TIAGO
-          <br />
-          <span className="opacity-0">
-            _<br />_
-          </span>
-        </h1>
-      </div>
+      <Hero />
 
       <div className="h-[300vh]"></div>
     </>
