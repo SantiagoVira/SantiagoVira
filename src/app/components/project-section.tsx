8b8f16e1b98d912d50171cf3867c2990a583c784
SantiagoVira <santiagovira06@gmail.com>
2024-02-05 12:56:15 -0500

    setup projects section

---
+++ src/app/components/project-section.tsx
@@ -0,0 +1,9 @@
+const Projects: React.FC = () => {
+  return (
+    <div className="flex flex-col relative w-full justify-start items-center">
+      <h2 className="font-dunk text-6xl text-accent">PROJECTS</h2>
+    </div>
+  );
+};
+
+export default Projects;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about-section";
 import Hero from "./components/hero";
+import Projects from "./components/project-section";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
       <div className="h-[300vh]"></div>
     </>
   );
