a75549aaef8b11d644a604b9eb5f052c69baadc9
SantiagoVira <santiagovira06@gmail.com>
2024-02-04 22:21:28 -0500

    finish curve

---
+++ src/app/components/quadratic-bottom.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useCallback, useEffect, useRef } from "react";
+
+const QuadraticBottom: React.FC = () => {
+  const path = useRef<SVGPathElement>(null);
+
+  const setPath = useCallback(
+    (controlXRatio: number, controlYRatio: number) => {
+      const screenWidth = window.innerWidth;
+
+      if (path.current)
+        path.current.setAttributeNS(
+          null,
+          "d",
+          `M 0 0 Q ${controlXRatio * screenWidth} ${
+            controlYRatio * 100
+          } ${screenWidth} 0`
+        );
+    },
+    []
+  );
+
+  useEffect(() => {
+    setPath(0.5, 0.5);
+
+    window.addEventListener("resize", () => {
+      setPath(0.5, 0.5);
+    });
+
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      setPath(clientX / window.innerWidth, clientY / window.innerHeight);
+    });
+  }, [setPath]);
+
+  return (
+    <svg height="100" className="w-full" xmlns="http://www.w3.org/2000/svg">
+      <path ref={path} className="stroke-1 stroke-accent fill-accent"></path>
+    </svg>
+  );
+};
+
+export default QuadraticBottom;
--- src/app/components/section1.tsx
+++ src/app/components/section1.tsx
@@ -7,39 +7,20 @@ import {
   useMotionValueEvent,
 } from "framer-motion";
 import Image from "next/image";
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import Bread1 from "@/images/bread1.jpeg";
 import Bread2 from "@/images/bread2.jpeg";
 import Bread3 from "@/images/bread3.jpeg";
+import QuadraticBottom from "./quadratic-bottom";
 
 const Section1: React.FC = () => {
   const container = useRef(null);
-
-  const path = useRef<SVGPathElement>(null);
-  let progress = 0;
-  let x = 0.5;
-
-  const setPath = useCallback(
-    (value: number) => {
-      const width = window.innerWidth * 0.7;
-
-      if (path.current)
-        path.current.setAttributeNS(
-          null,
-          "d",
-          `M 0 50 Q ${width * x} ${50 + value} ${width} 50`
-        );
-    },
-    [x]
-  );
-
-  useEffect(() => {
-    setPath(progress);
-
-    window.addEventListener("resize", () => {
-      setPath(progress);
-    });
-  }, [progress, setPath]);
 
   const images = [Bread1, Bread2, Bread3];
   const [imageIdx, setImageIdx] = useState(0);
@@ -56,7 +37,7 @@ const Section1: React.FC = () => {
 
   return (
     <div className="flex flex-col relative w-full justify-start items-center">
-      <div className="border-t-[5vw] border-t-transparent border-l-[50vw] border-l-transparent border-r-[50vw] border-accent border-b-[10vh] w-0 h-0" />
+      <div className="border-t-[8vh] border-t-transparent border-l-[50vw] border-l-transparent border-r-[50vw] border-accent border-b-[8vh] w-0 h-0" />
       <div
         className="flex flex-col relative w-full justify-start h-[300vh] items-center"
         ref={container}>
@@ -72,30 +53,7 @@ const Section1: React.FC = () => {
           />
         </div>
       </div>
-      <div className="h-[1px] relative w-full mb-5">
-        <span className="h-[40px] w-full flex relative -top-5 z-10"></span>
-
-        <svg
-          height="100"
-          className="absolute h-[100px] w-full"
-          xmlns="http://www.w3.org/2000/svg">
-          <path ref={path} className="stroke-1 stroke-accent"></path>
-        </svg>
-      </div>
-      {/* <svg height="400" width="450" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M 100 250 Q 250 180 400 250"
-          className="fill-none stroke-green-500 stroke-[3]"
-        />
-      </svg>
-      <svg height="400" width="450" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M 100 350 q 150 -300 300 0"
-          stroke="blue"
-          stroke-width="4"
-          fill="none"
-        />
-      </svg> */}
+      <QuadraticBottom />
     </div>
   );
 };
