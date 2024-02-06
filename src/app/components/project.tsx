e42a9416679592e9d8f759291aaafdccde97bfb6
SantiagoVira <santiagovira06@gmail.com>
2024-02-05 22:49:52 -0500

    font fiddling

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -9,14 +9,14 @@ const Project: React.FC<{
   return (
     <div
       className={`p-2 flex items-start justify-between gap-24 rounded-lg w-full h-full transition-all duration-[400ms] ${
-        faded && "opacity-40"
+        faded && "opacity-30"
       } `}
       onMouseEnter={() => setHoverIdx(idx)}>
       <p className="text-accent/60 font-bold">2023</p>
       <div className="flex items-start justify-start flex-col flex-1 gap-1">
-        <p className="text-accent text-xs">FULLSTACK</p>
+        <p className="text-accent/80 text-xs">FULLSTACK</p>
         <h3 className="font-bold text-2xl text-accent">Hello, world!</h3>
-        <p className="text-accent">TEST TEST TEST TEST</p>
+        <p className="text-accent/75">TEST TEST TEST TEST</p>
       </div>
       <p className="text-9xl font-black text-accent">☃︎</p>
     </div>
