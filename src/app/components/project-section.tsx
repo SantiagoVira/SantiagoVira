dbb945e144a5fcae040ccc41cb21c14d13669cb0
SantiagoVira <santiagovira06@gmail.com>
2024-02-05 22:27:58 -0500

    project tabs

--- src/app/components/project-section.tsx
+++ src/app/components/project-section.tsx
@@ -12,25 +12,55 @@ const Projects: React.FC = () => {
     [1, 1],
   ];
   const [hoverIdx, setHoverIdx] = useState(-1);
+  const [tabIdx, setTabIdx] = useState(0);
+
+  const tabs = ["Top Projects", "Design Work", "Project Archive"];
   return (
-    <div className={`flex flex-col relative w-full justify-start items-center`}>
+    <div
+      className="flex flex-col w-full justify-start items-center max-w-[60rem] gap-4"
+      onMouseLeave={() => setHoverIdx(-1)}>
       <h2 className="font-dunk text-6xl text-accent mb-5">PROJECTS</h2>
-      <div
-        className="grid grid-cols-3 grid-rows-3 w-full h-[45rem] max-w-[60rem] gap-4"
-        onMouseLeave={() => setHoverIdx(-1)}>
-        {projects.map((project, i) => (
+      <div className="flex w-full">
+        {tabs.map((tab, i) => (
+          <Tab active={tabIdx === i} setTabIdx={setTabIdx} idx={i} key={i}>
+            {tab}
+          </Tab>
+        ))}
+      </div>
+      {projects.map((project, i) => (
+        <>
           <Project
             setHoverIdx={setHoverIdx}
-            cols={project[0]}
-            rows={project[1]}
             idx={i}
             faded={hoverIdx !== -1 && hoverIdx !== i}
             key={i}
           />
-        ))}
-      </div>
+          <hr className=" border-accent" />
+        </>
+      ))}
     </div>
   );
 };
 
 export default Projects;
+
+const Tab: React.FC<
+  React.PropsWithChildren<{
+    setTabIdx: React.Dispatch<React.SetStateAction<number>>;
+    idx: number;
+    active?: boolean;
+  }>
+> = ({ setTabIdx, idx, active = false, children }) => (
+  <div
+    className={`flex flex-1 items-center justify-center transition-all pt-6 hover:pt-4 group border-b   ${
+      active ? "border-accent" : "border-accent/30 hover:border-accent/65"
+    }`}
+    onClick={() => setTabIdx(idx)}>
+    <p
+      className={`transition-all  pb-1 ${
+        active ? "text-accent" : "text-accent/30 group-hover:text-accent/65"
+      }`}>
+      {children}
+    </p>
+  </div>
+);
--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -1,16 +1,14 @@
 const Project: React.FC<{
   setHoverIdx: React.Dispatch<React.SetStateAction<number>>;
   idx: number;
-  rows: number;
-  cols: number;
   faded?: boolean;
-}> = ({ setHoverIdx, idx, rows, cols, faded = false }) => {
+}> = ({ setHoverIdx, idx, faded = false }) => {
   "row-span-3";
   return (
     <div
-      className={`p-2 flex flex-col items-center justify-center gap-4 hover:bg-accent/10 rounded-lg border-accent border w-full h-full transition-all duration-[400ms] ${
+      className={`p-2 flex items-start justify-center gap-4 hover:bg-accent/10 rounded-lg border-accent border w-full h-full transition-all duration-[400ms] ${
         faded && "opacity-40"
-      } col-span-${cols} row-span-${rows}`}
+      } `}
       onMouseEnter={() => setHoverIdx(idx)}>
       <p className="text-9xl font-black text-accent">☃︎</p>
       <h3 className="font-bold text-2xl text-accent">Hello, world!</h3>
