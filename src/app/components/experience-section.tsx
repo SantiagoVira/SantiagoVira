14e51aab04704b7b70dba854ab583fe3bfcfcd6d
SantiagoVira <santiagovira06@gmail.com>
2024-07-22 17:27:16 -0400

    add horizontal scroll in experience

--- src/app/components/experience-section.tsx
+++ src/app/components/experience-section.tsx
@@ -1,28 +1,29 @@
 "use client";
 
-import Image from "next/image";
-import Bread1 from "@/images/bread1.jpeg";
-import QuadraticBottom from "./quadratic-bottom";
-import PortableTextLayout from "@/utils/portable-text";
+import { useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 const ExperienceSection: React.FC<{ text: any[] }> = ({ text }) => {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+
+    offset: ["start start", "end end"],
+  });
+
+  const xPercent = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const x = useTransform(xPercent, (p) => `${p}%`);
+
   return (
-    <div className="flex flex-col relative w-full justify-start items-center">
-      <div className="border-t-[8vh] border-t-transparent border-l-[50vw] border-l-transparent border-r-[50vw] border-accent border-b-[8vh] w-0 h-0" />
-      <div className="flex flex-col relative w-full justify-start h-screen items-center">
-        <div className="h-screen sticky top-0 w-full flex items-start gap-20 justify-between p-24 bg-accent">
-          <div className="flex flex-col text-background text-xl gap-2">
-            <h2 className="font-dunk text-6xl">ABOUT</h2>
-            <PortableTextLayout text={text} />
-          </div>
-          <Image
-            className="min-w-[30vw] h-[80vh] object-cover"
-            src={Bread1}
-            alt="image"
-          />{" "}
-        </div>
+    <div ref={container} className="h-[300vh] relative w-full">
+      <div className="sticky overflow-hidden top-0 h-screen">
+        <motion.div
+          style={{ x }}
+          className="w-fit h-full absolute top-0 flex items-center justify-start pl-10">
+          <p className="text-[30rem]">EXPERIENCE</p>
+        </motion.div>
       </div>
-      <QuadraticBottom />
     </div>
   );
 };
