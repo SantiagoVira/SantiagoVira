3fc5ce5a62745c5eb87ca940b02ad144771c90a4
SantiagoVira <santiagovira06@gmail.com>
2024-07-22 00:07:42 -0400

    i really like this footer

--- src/app/components/contacts-section.tsx
+++ src/app/components/contacts-section.tsx
@@ -6,28 +6,48 @@ import santiagoCircle from "@/images/santiago-circle.svg";
 import Image from "next/image";
 import Link from "next/link";
 import UnderlineHover from "./underline-hover";
-import { chooseRandomTidbit } from "./tidbit-box";
-import { ChangingTidbit } from "./hero";
 
 const ContactsSection: React.FC = () => {
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: parentRef,
+
+    offset: ["start end", "end end"],
+  });
+
+  const clipPath = useTransform(scrollYProgress, (y) => {
+    const mult = 125;
+    const topY = 50 - y * mult;
+    const rightX = 50 + y * mult;
+    const bottomY = 50 + y * mult;
+    const leftX = 50 - y * mult;
+    return `polygon(50% ${topY}%, ${rightX}% 50%, 50% ${bottomY}%, ${leftX}% 50%)`;
+  });
+
   return (
-    <div className="bg-darkBackground w-full relative py-20 mt-32">
-      <div className="w-52 aspect-square absolute -top-20 -left-20">
-        <Image
-          src={santiagoCircle}
-          alt="Santiago Vira"
-          className="animate-[spin_4s_linear_infinite]"
-        />
+    <motion.div
+      ref={parentRef}
+      className="w-full mt-32 h-[15.5rem]"
+      style={{ clipPath }}>
+      <div className="h-[15.5rem] fixed w-full bottom-0 bg-darkBackground flex flex-col justify-center">
+        <div className="w-52 aspect-square absolute -top-20 -left-20">
+          <Image
+            src={santiagoCircle}
+            alt="Santiago Vira"
+            className="animate-[spin_4s_linear_infinite]"
+          />
+        </div>
+        <h3 className="font-dunk w-full text-center text-6xl">REACH OUT</h3>
+        <div className="w-80 ml-auto mr-auto flex justify-center text-xl gap-2 items-center">
+          <CustomLink href="https://www.linkedin.com/in/santiago-vira/">
+            LinkedIn
+          </CustomLink>
+          <CustomLink href="https://github.com/SantiagoVira">GitHub</CustomLink>
+          <CustomLink href="mailto:santiagovira06@gmail.com">Email</CustomLink>
+        </div>
       </div>
-      <h3 className="font-dunk w-full text-center text-6xl">REACH OUT</h3>
-      <div className="w-80 ml-auto mr-auto flex justify-center text-xl gap-2 items-center">
-        <CustomLink href="https://www.linkedin.com/in/santiago-vira/">
-          LinkedIn
-        </CustomLink>
-        <CustomLink href="https://github.com/SantiagoVira">GitHub</CustomLink>
-        <CustomLink href="mailto:santiagovira06@gmail.com">Email</CustomLink>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
