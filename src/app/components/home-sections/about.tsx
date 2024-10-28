3321ab3dd9cd61f6191ca40b316980ce69a94434
SantiagoVira <santiagovira06@gmail.com>
2024-10-28 16:49:05 -0400

    moved some things around

--- src/app/components/home-sections/about.tsx
+++ src/app/components/home-sections/about.tsx
@@ -1,88 +1,132 @@
 "use client";
 
-import Image from "next/image";
-
 import QuadraticBottom from "@components/quadratic-bottom";
 import PortableTextLayout from "@utils/portable-text";
 import { FileType } from "@/sanity-helpers";
 import useWindowSize from "@/utils/use-window-size";
-import { useScroll } from "framer-motion";
-import { useEffect, useRef } from "react";
+import {
+  motion,
+  MotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import { useLayoutEffect, useRef, useState } from "react";
 import SectionHeader from "../ui/section-header";
+
+import Bread1 from "@images/bread1.jpeg";
+import Bread2 from "@images/bread2.jpeg";
+import Bread3 from "@images/bread3.jpeg";
 
 const AboutSection: React.FC<{ text: any[]; portraitData: FileType }> = ({
   text,
   portraitData,
 }) => {
-  const { isDesktop, windowSize } = useWindowSize();
-  const parent = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: parent,
-    offset: ["start end", "end end"],
-  });
-  const topSlidingText = useRef<SVGTextPathElement>(null);
-  const topTextPath = useRef<SVGPathElement>(null);
-  const bottomSlidingText = useRef<SVGTextPathElement>(null);
-  const bottomTextPath = useRef<SVGPathElement>(null);
+  // STATE, REFS & EFFECTS
+  const [scrollRange, setScrollRange] = useState(0);
+  const container = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    scrollYProgress.on("change", (v) => {
-      const topVal = -50 + 150 * v;
-      const bottomVal = 46 / 821 + topVal;
-      topSlidingText.current?.setAttribute("startOffset", `${topVal}%`);
-      bottomSlidingText.current?.setAttribute("startOffset", `${bottomVal}%`);
+  useLayoutEffect(() => {
+    content.current?.scrollWidth && setScrollRange(content.current.scrollWidth);
+  }, [content]);
+
+  // HOOK CALLS
+  const {
+    isDesktop,
+    windowSize: { width: viewportWidth },
+  } = useWindowSize();
+
+  // FRAMER MOTION
+  const useMoveElement = (
+    startCoefficient: number,
+    endCoefficient: number,
+    startOffset: number = 0
+  ) => {
+    const { scrollYProgress } = useScroll({
+      target: container,
+      offset: ["start start", "end end"],
     });
-  }, [scrollYProgress, windowSize]);
+
+    const transform = useTransform(
+      scrollYProgress,
+      [0, 1],
+      [
+        startOffset + startCoefficient * (viewportWidth ?? 0),
+        endCoefficient * (viewportWidth ?? 0),
+      ]
+    );
+
+    const physics = { damping: 15, mass: 0.27, stiffness: 55 };
+    return useSpring(transform, physics);
+  };
+
+  const titleTransform = useMoveElement(1, 0, -scrollRange);
+  const descTransform = useMoveElement(1, -0.5);
+  const img1Transform = useMoveElement(0.8, 0);
+  const img2Transform = useMoveElement(1.2, -1.5);
+  const img3Transform = useMoveElement(2, -1);
+  const img4Transform = useMoveElement(2, 0.4);
 
   return (
-    <div
-      className="flex flex-col relative w-full justify-start items-center"
-      id="about">
-      <div className="border-t-[8vh] border-t-transparent border-l-[50vw] border-l-transparent border-r-[50vw] border-accent border-b-[8vh] w-0 h-0" />
+    <>
       <div
-        className="flex flex-col relative w-full justify-start h-fit md:h-screen items-center"
-        ref={parent}>
-        <div className="h-full w-full flex flex-col items-center gap-20 justify-start py-24 bg-accent">
-          <div className="flex flex-col text-background text-lg md:text-xl gap-2 max-w-[60rem] mx-12">
-            <SectionHeader variant="dark">ABOUT</SectionHeader>
-            {/* <PortableTextLayout text={text} /> */}
-          </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 546 138"
-            className="overflow-visible w-full"
-            fill="none">
-            <text>
-              <textPath
-                ref={topSlidingText}
-                href="#topCurve"
-                className="fill-background text-xs"
-                startOffset={"-50%"}>
-                I&apos;m designing a new way forward
-              </textPath>
-              <textPath
-                ref={bottomSlidingText}
-                href="#bottomCurve"
-                className="fill-background text-xs"
-                startOffset={"-50%"}>
-                and I hope you&apos;ll join me.
-              </textPath>
-            </text>
-            <path
-              className="w-full"
-              ref={topTextPath}
-              id="topCurve"
-              d="M1.00034 110.5C1.00034 110.5 96.5 97.5459 96.5 51.5C96.5 22.0001 73.8902 0.651084 46.0002 0.499988C18.5158 0.351089 1.0001 22 1.00024 48.5C1.0005 94.9482 56.0521 110.5 102.5 110.5C207.5 110.5 477.501 110.5 477.501 110.5"
-            />
-            <path
-              xmlns="http://www.w3.org/2000/svg"
-              ref={bottomTextPath}
-              id="bottomCurve"
-              d="M 22.5,137.001 L 69.0001,137.001 C 69.0001,137.001 164.5,124.047 164.5,78.0008 C 164.5,48.5009 141.89,27.1519 114.0,27.0008 C 86.5156,26.8519 68.9999,48.5008 69.0,75.0008 C 69.0003,121.449 124.052,137.001 170.5,137.001 C 275.5,137.001 545.5,137.001 545.5,137.001"
-            />
-          </svg>
-          <div className="h-[20rem] w-full"></div>
-          {/* {false && (
+        ref={container}
+        className="h-[300vh] relative w-full"
+        id="experience">
+        <div className="sticky overflow-hidden top-0 h-screen">
+          <motion.div
+            style={{ x: titleTransform }}
+            ref={content}
+            className="w-fit h-full absolute top-0 flex items-center justify-start">
+            <SectionHeader variant="light">ABOUT ME</SectionHeader>
+          </motion.div>
+          <motion.img
+            className="object-cover w-48 absolute top-[15%] -z-10 rounded-lg"
+            style={{ x: img1Transform, rotate: "6deg" }}
+            src={portraitData.link}
+            alt="Me :)"
+          />
+          <motion.img
+            className="object-cover w-32 absolute top-2/3 -z-10 rounded-lg"
+            style={{ x: img2Transform, rotate: "-22deg" }}
+            src={"/img/bread1.jpeg"}
+            alt="image"
+          />
+          <motion.img
+            className="object-cover w-32 absolute top-2/3 -z-10 rounded-lg"
+            style={{ x: img3Transform, rotate: "0deg" }}
+            src={"/img/bread2.jpeg"}
+            alt="image"
+          />
+          <motion.img
+            className="object-cover w-32 absolute top-2/3 -z-10 rounded-lg"
+            style={{ x: img4Transform, rotate: "-5deg" }}
+            src={"/img/bread3.jpeg"}
+            alt="image"
+          />
+          <motion.p
+            className="absolute top-[55%] font-medium text-lg max-w-fit whitespace-nowrap"
+            style={{ x: descTransform }}>
+            I create elegant, modern designs for everyday use. My full stack
+            development experience allows me to craft your ideas into reality. I
+            hope you will join me for the journey.
+          </motion.p>
+        </div>
+      </div>
+      {/* <div
+        className="flex flex-col relative w-full justify-start items-center"
+        id="about">
+        <div className="border-t-[8vh] border-t-transparent border-l-[50vw] border-l-transparent border-r-[50vw] border-accent border-b-[8vh] w-0 h-0" />
+        <div className="flex flex-col relative w-full justify-start h-fit md:h-screen items-center">
+          <div className="h-full w-full flex flex-col items-center gap-20 justify-start py-24 bg-accent">
+            <div className="flex flex-col text-background text-lg md:text-xl gap-2 max-w-[60rem] mx-12">
+              <SectionHeader variant="dark">ABOUT</SectionHeader>
+              <PortableTextLayout text={text} />
+            </div>
+
+            <div className="h-[20rem] w-full"></div>
+             {false && (
             <div className="min-w-[30vw] w-full h-[80vh] relative rounded-lg overflow-hidden">
               <Image
                 className="object-cover"
@@ -91,11 +135,12 @@ const AboutSection: React.FC<{ text: any[]; portraitData: FileType }> = ({
                 alt="image"
               />
             </div>
-          )} */}
+          )}
+          </div>
         </div>
-      </div>
-      {isDesktop && <QuadraticBottom />}
-    </div>
+        {isDesktop && <QuadraticBottom />}
+      </div> */}
+    </>
   );
 };
 
--- src/app/components/home-sections/projects.tsx
+++ src/app/components/home-sections/projects.tsx
@@ -17,12 +17,14 @@ const ProjectsSection: React.FC<{
         className="flex flex-col w-full justify-start items-center max-w-[60rem] gap-4 pt-10"
         id="projects">
         <div className="w-screen px-12 mb-5">
-          <SectionHeader className="text-center">TOP PROJECTS</SectionHeader>
+          <SectionHeader className="font-dunk text-4xl md:text-6xl text-accent text-center">
+            TOP PROJECTS
+          </SectionHeader>
         </div>
 
         <ProjectList projects={topProjects} />
       </div>
-      <div className="w-screen mt-2">
+      <div className="w-screen mt-2 max-w-[60rem] px-12">
         <Marquee autoFill={true} pauseOnClick>
           <div className="gap-2 mx-1 uppercase flex">
             <UnderlineHover>
